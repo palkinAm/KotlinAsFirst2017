96e57dbdce8da3f73826b6b945fd454667751e51
Александр <palkin99@mail.ru>
2017-09-20 02:28:36 +0300

    6 zadach

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -60,7 +60,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (hours*3600)+(minutes
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =((vershoks*4.445)+(arshins*16*4.445)+(sagenes*48*4.445))/100
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =((vershoks*4.445)+(arshins*16*4.445)+
+        (sagenes*48*4.445))/100
 
 /**
  * Тривиальная
@@ -84,7 +85,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt((
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = (number%1000)/100
 
 /**
  * Простая
@@ -93,7 +94,8 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = ((hoursArrive*60)+
+minutesArrive)-((hoursDepart*60)+minutesDepart)
 
 /**
  * Простая
