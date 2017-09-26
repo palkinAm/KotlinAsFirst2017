0b332b9ab8771ba4c0777fcc963ae53133673b53
Александр <palkin99@mail.ru>
2017-09-26 23:15:53 +0300

    3 задача 2 урока

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -58,9 +58,9 @@ fun timeForHalfWay(t1: Double, v1: Double,
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
                        rookX2: Int, rookY2: Int): Int =
-        if ((kingX != rookX1 && kingY !=rookY1) && (kingX != rookX2 && kingY != rookY2) ) 0 else
-            if (kingX == rookX1 || kingY == rookY1) 1 else
-                if (kingX == rookX2 || kingY == rookY2) 2 else 3
+        if ((kingX != rookX1) && (kingY != rookY1) && (kingX != rookX2) && (kingY != rookY2)) 0 else
+            if ((kingX == rookX1 || kingY == rookY1) && (kingX != rookX2 && kingY != rookY2)) 1 else
+                if ((kingX == rookX2 || kingY == rookY2) && (kingX != rookX1 && kingY != rookY1)) 2 else 3
 
 /**
  * Простая
