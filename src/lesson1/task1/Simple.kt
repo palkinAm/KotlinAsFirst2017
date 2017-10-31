b87e7079e11efa866e0249cc4319842cd0d68346
Александр <palkin99@mail.ru>
2017-10-31 20:51:17 +0300

    3 урок, сложная задача

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -71,7 +71,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
 fun angleInRadian(grad: Int, min: Int, sec: Int): Double =
-        (grad + (min / 60.0) + (sec / 3600.0)) * PI / 180.
+        (grad + (min / 60.0) + (sec / 3600.0)) * PI / 180.0
 
 /**
  * Тривиальная
